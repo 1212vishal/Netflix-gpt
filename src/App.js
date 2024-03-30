e518f7657e60e9092afd1fef273d342156bff5d1
vishal kumar <vkumar971660@gmail.com>
2024-03-30 17:15:25 +0530

    just chek

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="text-3xl font-bold text-yellow-400">
-      Hello World!  
+      Hello World!  This is my netflix app. 
     </div>
   );
 }
